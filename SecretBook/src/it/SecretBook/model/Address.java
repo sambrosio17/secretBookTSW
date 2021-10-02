c1da0c0869ef3be3dc23c50af18ad87b70750459
sambrosio17 <ambrosio.s@outlook.it>
2021-10-02 17:38:14 +0200

    Sat Oct 02 17:38:14 CEST 2021 - #Tangled - Rimozione automatica dei file - commit #4

---
+++ SecretBook/src/it/SecretBook/model/Address.java
@@ -0,0 +1,81 @@
+package it.SecretBook.model;
+
+public class Address {
+
+	private int id;
+	private String nazione;
+	private String citta;
+	private String via;
+	private int cap;
+	private int num;
+	private String utente;
+
+	public Address() {
+		id = -1;
+		nazione = "";
+		citta = "";
+		via = "";
+		cap = -1;
+		num = -1;
+		utente="";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNazione() {
+		return nazione;
+	}
+
+	public void setNazione(String nazione) {
+		this.nazione = nazione;
+	}
+
+	public String getCitta() {
+		return citta;
+	}
+
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+
+	public String getVia() {
+		return via;
+	}
+
+	public void setVia(String via) {
+		this.via = via;
+	}
+
+	public int getCap() {
+		return cap;
+	}
+
+	public void setCap(int cap) {
+		this.cap = cap;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getUtente() {
+		return utente;
+	}
+
+	public void setUtente(String utente) {
+		this.utente = utente;
+	}
+
+	
+}
+//Untangled partizione: 15
