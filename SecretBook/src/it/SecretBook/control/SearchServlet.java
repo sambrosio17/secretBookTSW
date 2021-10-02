331bb4c5c94294461d13d9043d04a53360e2bd3b
sambrosio17 <ambrosio.s@outlook.it>
2021-10-02 17:38:33 +0200

    Sat Oct 02 17:38:33 CEST 2021 - #Tangled - Rimozione automatica dei file - commit #15

---
+++ SecretBook/src/it/SecretBook/control/SearchServlet.java
@@ -0,0 +1,67 @@
+package it.SecretBook.control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.SecretBook.DAO.ProductModel;
+import it.SecretBook.model.Product;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/search")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      
+	ProductModel pModel= new ProductModel();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String search= request.getParameter("q");
+		
+		request.setAttribute("queryString", search);
+		
+		LinkedList<Product> list=new LinkedList<Product>();
+		
+		try {
+			list=(LinkedList<Product>) pModel.doRetrieveFromSearch(search);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		request.setAttribute("result", list);
+		
+		RequestDispatcher rd=getServletContext().getRequestDispatcher("/searchResult.jsp");
+		rd.forward(request, response);
+		return;
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+//Untangled partizione: 11
