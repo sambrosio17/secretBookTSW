e484cc900ed567dcf0634ca658047774cb6fcc42
sambrosio17 <ambrosio.s@outlook.it>
2021-10-02 17:38:10 +0200

    Sat Oct 02 17:38:10 CEST 2021 - #Tangled - Rimozione automatica dei file - commit #2

---
+++ SecretBook/src/it/SecretBook/control/UserPictureServlet.java
@@ -0,0 +1,66 @@
+package it.SecretBook.control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.SecretBook.DAO.UserModel;
+
+/**
+ * Servlet implementation class UserPictureServlet
+ */
+@WebServlet("/userPicture")
+public class UserPictureServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	UserModel model=new UserModel();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserPictureServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username=request.getParameter("username");
+		if (username!=null && !username.equals(""))
+		{
+			byte[] bt=null;
+			try {
+				bt = model.load(username);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+			ServletOutputStream out = response.getOutputStream();
+			if(bt != null)
+			{
+				out.write(bt);
+				response.setContentType("image/jpeg");			
+			}
+			out.close();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+//Untangled partizione: 13
